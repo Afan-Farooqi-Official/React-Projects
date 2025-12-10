645a274ab43cd2e965e0d54b7afbe52710ed3d05
Afan-Farooqi-Official <affanqaisar000@gmail.com>
2025-12-10 10:50:46 +0500

    quiz app logic implementation

--- Quiz-App/src/App.jsx
+++ Quiz-App/src/App.jsx
@@ -1,36 +1,79 @@
 import './App.css'
+import questions from './Questions'
+import { useState } from 'react'
 
 function App() {
+
+  const [currentQ, setcurrentQ] = useState(0)
+  const [selected, setSelected] = useState(null)
+  const [score, setScore] = useState(0)
+
+  const handleOptionChange = (index) => {
+    setSelected(index)
+  };
+
+  const handleNext = () => {
+    if (selected === questions[currentQ].answer) {
+      setScore(score+1)
+    }
+    setSelected(null);
+
+    if (currentQ + 1 < questions.length) {
+      setcurrentQ(currentQ+1)
+    } else {
+      setcurrentQ(questions.length)
+    }
+  }
+
+  const handlePrev = () => {
+    if (currentQ > 0) {
+      setcurrentQ(currentQ-1)
+    }
+  }
+
+  const handleReset = () => {
+    setcurrentQ(0)
+    setSelected(null)
+    setScore(0)
+  }
 
   return (
     <div class='bg-gray-100 w-4xl h-[90vh] flex m-auto mt-4 rounded-2xl flex-col overflow-hidden'>
       <div class='flex h-16 bg-blue-400'>
-        <div class='mt-5 ml-5 text-white'>
+        <div onClick={handlePrev} class='mt-5 ml-5 text-white transition duration-200 ease-in-out cursor-pointer'>
           <svg xmlns="http://www.w3.org/2000/svg" width="35" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-left-icon lucide-arrow-left"><path d="m12 19-7-7 7-7"/><path d="M19 12H5"/></svg>
         </div>
         <h1 class='ml-[40%] flex mt-3 text-white font-bold text-4xl'>Quiz</h1>
       </div>
-      <div>
+      {currentQ < questions.length ? (
+        <>
+        <div>
         <div class='bg-gray-50 w-11/12 mx-auto mt-5 rounded-lg px-6 py-5 shadow-md'>
-          <h2 class='font-bold text-lg'>Question 1</h2>
-          <p class='mt-3 text-base font-semibold'>What is the Capital of France?</p>
+          <h2 class='font-bold text-lg'>Question: {currentQ + 1} of {questions.length}</h2>
+          <p class='mt-3 text-base font-semibold'>{questions[currentQ].question}</p>
         </div>
-        <div class='w-[90%] m-auto mt-5 rounded-[6px] py-5 flex flex-col'>
-          <div class='w-full p-3 bg-amber-50 my-2 rounded-lg border border-gray-300 hover:bg-amber-100 hover:shadow-md transition-colors duration-300 ease-in-out cursor-pointer flex items-center'>
-            <input class=' cursor-pointer w-4 h-4' type="radio" value='london' name='option' />
-            <label class='ml-3 text-[20px]'>London</label>
-          </div>
-          <div class='w-full p-3 bg-amber-50 my-2 rounded-lg border border-gray-300 hover:bg-amber-100 hover:shadow-md transition-colors duration-300 ease-in-out cursor-pointer flex items-center'>
-            <input class=' cursor-pointer w-4 h-4' type="radio" value='rome' name='option' />
-            <label class='ml-3 text-[20px]'>Rome</label>
-          </div>
-          <div class='w-full p-3 bg-amber-50 my-2 rounded-lg border border-gray-300 hover:bg-amber-100 hover:shadow-md transition-colors duration-300 ease-in-out cursor-pointer flex items-center'>
-            <input class=' cursor-pointer w-4 h-4' type="radio" value='berlin' name='option' />
-            <label class='ml-3 text-[20px]'>Berlin</label>
-          </div>
+        <div class='w-[90%] m-auto mt-5 rounded-md py-5 flex flex-col'>
+          {questions[currentQ].options.map((opt, index) => (
+            <label key={index} class={`flex items-center p-3 rounded-lg border cursor-pointer transition my-1.5 ${selected === index ? "bg-blue-100 border-blue-400": "bg-white border-gray-200 hover:bg-gray-50"}`}>
+            <input type="radio" name="option" checked={selected === index} onChange={()=>handleOptionChange(index)} class="w-4 h-4 text-blue-600 cursor-pointer" />
+              <span class="ml-3 text-lg text-gray-800">{opt}</span>
+            </label>
+          ))}
         </div>
       </div>
-      <button class='mt-4 bg-blue-400 w-40 h-10 m-auto text-white text-[20px] font-semibold rounded-[8px] cursor-pointer hover:bg-blue-500 hover:scale-105 transition-all duration-300 ease-in-out'>Next</button>
+        <button onClick={handleNext} disabled={selected===null} class='mt-4 mx-auto w-40 h-12 bg-blue-500 text-white text-lg font-semibold rounded-lg cursor-pointer hover:bg-blue-600 hover:scale-105 transition-colors duration-300 ease-in-out'>Next</button>
+        </>
+      ): (
+        <>
+          <div class='flex flex-col items-center mt-10'>
+            <h2 class='text-2xl font-bold'>Quiz Finished</h2>
+            <p class='mt-2 text-lg'>Your score: {score} / {questions.length}</p>
+          </div>
+          <button onClick={handleReset} class='bg-blue-400 text-white text-[20px] text-center mt-4 w-40 h-12 mx-auto rounded-sm hover:bg-blue-600 hover:scale-105 transition-colors duration-300 ease-in-out cursor-pointer'>
+            Reset
+          </button>
+        </>
+      )}
     </div>
   )
 }
---
+++ Quiz-App/src/Questions.jsx
@@ -0,0 +1,35 @@
+
+const questions = [
+    {
+        question: 'Which planet is known as Red Planet?',
+        options: ['Venus', 'Saturn', 'Mars', 'Jupiter'],
+        answer: 2, //Mars
+    },
+    {
+    question: "Who wrote the play 'Romeo and Juliet'?",
+    options: ["William Shakespeare", "Jane Austen", "Mark Twain", "Charles Dickens"],
+    answer: 0, // Shakespeare
+  },
+  {
+    question: "What is the capital city of Japan?",
+    options: ["Nagoya", "Tokyo", "Kyoto", "Osaka"],
+    answer: 1, // Tokyo
+  },
+  {
+    question: "Which ocean is the largest on Earth?",
+    options: ["Indian Ocean", "Atlantic Ocean", "Arctic Ocean", "Pacific Ocean"],
+    answer: 3, // Pacific Ocean
+  },
+  {
+    question: "In computing, what does 'CPU' stand for?",
+    options: [
+      "Control Program Unit",
+      "Central Processing Unit",
+      "Core Processing Utility",
+      "Computer Power Utility",
+    ],
+    answer: 1, // Central Processing Unit
+  },
+]
+
+export default questions;
